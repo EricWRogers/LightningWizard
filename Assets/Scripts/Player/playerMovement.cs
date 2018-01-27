43ab8c8b36f57f0342a3f1df1cd67617b2258940
Eric <yerick147083@gmail.com>
2018-01-27 10:29:21 -0600

    Auto stash before merge of "master" and "origin/master"
    cube

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -8,16 +8,18 @@ public class playerMovement : MonoBehaviour {
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
-	void Update() {
-		CharacterController controller = GetComponent<CharacterController>();
+
+	void FixedUpdate() {
+		CharacterController controller = GetComponent<CharacterController> ();
 		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			//moveDirection = transform.TransformDirection (moveDirection);
 			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
+			if (Input.GetButton ("Jump"))
 				moveDirection.y = jumpSpeed;
 
 		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+		moveDirection.y = Physics.gravity.y * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
 }
