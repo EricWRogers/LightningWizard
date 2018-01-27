9266b86c8693dc3f2e30e89aef2e68238343fbd9
Unknown <TSAbrams3039@muleriders.saumag.edu>
2018-01-27 12:06:07 -0600

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-	public float speed = 5.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
-	private Vector3 moveDirection = Vector3.zero;
-
-	void Update() {
-		CharacterController controller = GetComponent<CharacterController> ();
-		if (controller.isGrounded) {
-			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-			moveDirection = transform.TransformDirection (moveDirection);
-			moveDirection *= speed;
-			if (Input.GetButton ("Jump"))
-				moveDirection.y = jumpSpeed;
-
-		}
-		moveDirection.y = Physics.gravity.y * Time.deltaTime;
-		controller.Move (moveDirection * Time.deltaTime);
-	}
+	
 }
